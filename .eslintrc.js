dcd4aa8aa29518c9af73a5864c7ed2a93901a170
Sergii Tkachenko <hi@sergii.org>
2018-08-31 03:48:51 -0400

    No default exports

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
   'extends': 'airbnb-base',
+  'rules': {
+    // https://blog.neufund.org/why-we-have-banned-default-exports-and-you-should-do-the-same-d51fdc2cf2ad
+    'import/prefer-default-export': 'off',
+    'import/no-default-export': 'error',
+  },
 }
