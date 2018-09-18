87b7e7e32449d6ae3ebfc189a18a37b3ffab6839
Sergii Tkachenko <hi@sergii.org>
2018-09-18 04:51:09 -0400

    Display schedules

--- pagerbeauty.mjs
+++ pagerbeauty.mjs
@@ -4,21 +4,13 @@ import dotenv from 'dotenv';
 
 // ------- Internal imports ----------------------------------------------------
 
-import { PagerDutyClient } from './src/services/PagerDutyClient';
-import { SchedulesService } from './src/services/SchedulesService';
 import { PagerBeautyWebApp } from './src/app/PagerBeautyWebApp';
 
 // ------- Program -------------------------------------------------------------
 
 dotenv.config();
 
-const pd = new PagerDutyClient(process.env.PD_API_KEY);
-const sss = new SchedulesService(pd);
-
-
-sss.load(process.env.PDS_CHEDULES.split(',')).then(res => console.log(res)).catch(e => console.log(e));
-
-// const webApp = new PagerBeautyWebApp();
-// webApp.start();
+const webApp = new PagerBeautyWebApp();
+webApp.start();
 
 // ------- End -----------------------------------------------------------------
--- src/controllers/SchedulesController.mjs
+++ src/controllers/SchedulesController.mjs
@@ -2,6 +2,9 @@
 
 // ------- Internal imports ----------------------------------------------------
 
+import { PagerDutyClient } from '../services/PagerDutyClient';
+import { SchedulesService } from '../services/SchedulesService';
+
 // ------- Class ---------------------------------------------------------------
 
 export class SchedulesController {
@@ -9,18 +12,27 @@ export class SchedulesController {
     // Bind web methods to object context so they can be passed to router.
     this.index = this.index.bind(this);
     this.show = this.show.bind(this);
+    this.schedulesService = false;
+  }
+
+  async init() {
+    const pagerDutyClient = new PagerDutyClient(process.env.PD_API_KEY);
+    this.schedulesService = new SchedulesService(pagerDutyClient);
+
+    await this.schedulesService.load(process.env.PDS_CHEDULES.split(','));
   }
 
   async index(ctx) {
-    ctx.body = {
-      test: 'index',
-    };
+    ctx.body = this.schedulesService.serialize();
   }
 
   async show(ctx, scheduleId) {
-    ctx.body = {
-      test: `show ${scheduleId}`,
-    };
+    const schedule = this.schedulesService.onCallRepo.get(scheduleId);
+    if (!schedule) {
+      ctx.status = 404;
+      return;
+    }
+    ctx.body = schedule.serialize();
   }
 }
 
