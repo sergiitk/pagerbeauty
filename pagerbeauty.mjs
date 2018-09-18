f6e61bdf27d592bbbd81b843fb9260cf4c767ea7
Sergii Tkachenko <hi@sergii.org>
2018-09-18 00:49:32 -0400

    WIP

--- pagerbeauty.mjs
+++ pagerbeauty.mjs
@@ -1,6 +1,22 @@
 // ------- Imports -------------------------------------------------------------
 
+import dotenv from 'dotenv';
+
+// ------- Internal imports ----------------------------------------------------
+
+import { PagerDutyService } from './src/services/PagerDutyService';
 import { PagerBeautyWebApp } from './src/app/PagerBeautyWebApp';
 
-const webApp = new PagerBeautyWebApp();
-webApp.start();
+// ------- Program -------------------------------------------------------------
+
+dotenv.config();
+
+const pd = new PagerDutyService();
+
+
+console.log(pd);
+
+// const webApp = new PagerBeautyWebApp();
+// webApp.start();
+
+// ------- End -----------------------------------------------------------------
