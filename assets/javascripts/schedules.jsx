71cf25d3aecdef796c082ccd9747d81bacc2f013
Sergii Tkachenko <hi@sergii.org>
2018-11-04 15:59:34 -0500

    Schedules list on react!

--- assets/javascripts/schedules.jsx
+++ assets/javascripts/schedules.jsx
@@ -8,19 +8,51 @@ const e = React.createElement;
 export class SchedulesList extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      schedules: []
+    };
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:8080/v1/schedules.json")
+      .then(res => res.json())
+      .then(
+        (schedules) => {
+          this.setState({ isLoaded: true, schedules });
+        },
+        (error) => {
+          this.setState({ isLoaded: true, error });
+        }
+      )
   }
 
   render() {
-    return <ul></ul>;
+    const { error, isLoaded, schedules } = this.state;
+    if (!isLoaded) {
+      return <span>Loading...</span>;
+    }
+    if (error) {
+      return <span>Loading error: {error}</span>;
+    }
+
+    const schedulesListItems = schedules.map((schedule) =>
+      <SchedulesListItem key={schedule.scheduleId} schedule={schedule} />
+    );
+
+    return <ul>{schedulesListItems}</ul>;
   }
 }
 
 export class SchedulesListItem extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
-    return <li>{this.props.scheduleName}</li>;
+    const schedule = this.props.schedule;
+    return (
+      <li>
+        <a href={`/v1/schedules/${schedule.scheduleId}.html`}>
+          {schedule.scheduleName}
+        </a>
+      </li>);
   }
 }
