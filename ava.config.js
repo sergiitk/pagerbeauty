57d15bf9de3495cc21405b37d094f663311f11ee
Sergii Tkachenko <hi@sergii.org>
2018-12-17 16:42:52 -0500

    Disable AVA's use of Babel.

--- ava.config.js
+++ ava.config.js
@@ -1,6 +1,8 @@
 // Ava config object
 const avaConfig = {
   require: ['esm'],
+  babel: false,
+  compileEnhancements: false,
 };
 
 export default avaConfig; /* eslint-disable-line import/no-default-export */
