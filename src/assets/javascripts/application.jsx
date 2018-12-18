fc4acbea09d05f35722f20da9545fe72c11a0f39
Sergii Tkachenko <hi@sergii.org>
2018-12-18 18:41:58 -0500

    Temporary use old-school refresh

--- src/assets/javascripts/application.jsx
+++ src/assets/javascripts/application.jsx
@@ -11,8 +11,14 @@ if (schedulesListRoot) {
   ReactDOM.render(<SchedulesList></SchedulesList>, schedulesListRoot);
 }
 
-
 // Render individual schedules
 document.querySelectorAll('.schedule_root').forEach((schedule) => {
   ReactDOM.render(<Schedule scheduleId={schedule.dataset.id}></Schedule>, schedule);
 })
+
+// Old-school refresh. To be replaced with React component states.
+window.onload = function() {
+  window.setTimeout(function() {
+    location.reload();
+  }, 60000); // 1 minute
+}
