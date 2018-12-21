b8fa66f24de747a3ce662f051401b2fc92dc656f
Sergii Tkachenko <hi@sergii.org>
2018-12-21 16:31:26 -0500

    Long polling using HOC

--- src/assets/javascripts/ui-backend-drivers.jsx
+++ src/assets/javascripts/ui-backend-drivers.jsx
@@ -6,11 +6,11 @@ import React from 'react';
 
 import { PagerBeautyHttpNotFoundUiError } from './ui-errors';
 
-// ------- OnCallLoaderView ----------------------------------------------------
+// ------- withAjaxBackend -----------------------------------------------------
 
 // HOC
-export function withAjaxBackend(WrappedComponent, endpoint) {
-  // ...and returns another component...
+export function withAjaxBackend(WrappedComponent, endpoint, pollIntervalSeconds=30) {
+  // Return wrapped component
   return class extends React.Component {
     constructor(props) {
       super(props);
@@ -20,9 +20,25 @@ export function withAjaxBackend(WrappedComponent, endpoint) {
         data: null,
         error: null,
       };
+      this.intervalId = false;
     }
 
     componentDidMount() {
+      // First load
+      this.loadData(endpoint);
+
+      // And start polling every pollIntervalSeconds;
+      const pollIntervalMs = pollIntervalSeconds * 1000;
+      this.intervalId = setInterval(() => {
+        this.loadData(endpoint);
+      }, pollIntervalMs);
+    }
+
+    componentWillUnmount() {
+      clearInterval(this.intervalId);
+    }
+
+    loadData() {
       fetch(endpoint)
         .then((response) => {
           if (!response.ok) {
@@ -31,7 +47,7 @@ export function withAjaxBackend(WrappedComponent, endpoint) {
           return response.json();
         })
         .then((data) => {
-          this.setState({ isLoading: false, data });
+          this.setState({ isLoading: false, error: null, data });
         })
         .catch((error) => {
           this.setState({ isLoading: false, error });
