c05247b66b0166db79f9907b0cbc8b8652e45992
Sergii Tkachenko <hi@sergii.org>
2018-12-24 22:48:56 -0500

    AVA lint

--- ava.config.js
+++ ava.config.js
@@ -1,5 +1,10 @@
 // Ava config object
 const avaConfig = {
+  files: [
+    'test/**/*.js',
+    'test/**/*.mjs',
+    '!test/helpers',
+  ],
   require: ['esm'],
   babel: false,
   compileEnhancements: false,
