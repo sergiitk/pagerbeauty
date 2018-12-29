985950db0575d90844194f174838183ac1b67ba3
Sergii Tkachenko <hi@sergii.org>
2018-12-29 18:52:56 -0500

    CircleCI: Human- and Machine-readable reports

--- test/run-acceptance-with-reports.sh
+++ test/run-acceptance-with-reports.sh
@@ -2,9 +2,13 @@
 
 set -o pipefail
 
-yarn -s run test:acceptance --tap | tee ./tmp/tap-ava.txt
+yarn -s run test:acceptance --tap > ./tmp/tap-ava.txt
 test_exit_code=$?
 
+# Human-readable diff
+FORCE_COLOR=t yarn -s run tap-diff < ./tmp/tap-ava.txt
+
+# Machine-readable xUnit report
 yarn -s run tap-xunit < ./tmp/tap-ava.txt > ./tmp/tap-xunit.xml
 
 exit $test_exit_code
