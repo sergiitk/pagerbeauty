8b96c060dea659568e90577d6c296daf407a50d7
Sergii Tkachenko <hi@sergii.org>
2018-12-30 04:39:23 -0500

    First unit test.

---
+++ test/unit/models/OnCall.test.mjs
@@ -0,0 +1,40 @@
+// ------- Imports -------------------------------------------------------------
+
+import test from 'ava';
+import chai from 'chai';
+
+// ------- Internal imports ----------------------------------------------------
+
+import { OnCall } from '../../../src/models/OnCall';
+
+// ------- Init ----------------------------------------------------------------
+
+const { expect } = chai;
+
+// ------- Tests ---------------------------------------------------------------
+
+test('OnCall', () => {
+  // TODO: fake properly.
+  const onCall = new OnCall({
+    scheduleId: 'scheduleId',
+    scheduleName: 'scheduleName',
+    scheduleURL: 'scheduleURL',
+    scheduleTimezone: 'scheduleTimezone',
+    userId: 'userId',
+    userName: 'userName',
+    userAvatarURL: 'userAvatarURL',
+    userURL: 'userURL',
+    dateStart: '2018-12-25T05:00:00.000Z',
+    dateEnd: '2018-12-25T17:00:00.000Z',
+  });
+
+  // Verify public API.
+  expect(onCall).to.respondTo('serialize');
+  expect(onCall).to.respondTo('toString');
+  expect(onCall).to.respondTo('userAvatarSized');
+
+  // Static.
+  expect(OnCall).itself.to.respondTo('fromApiRecord');
+});
+
+// ------- End -----------------------------------------------------------------
