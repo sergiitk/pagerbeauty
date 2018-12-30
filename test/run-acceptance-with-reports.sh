a43223710667a2c7a59f49e1974b4409fbabb55f
Sergii Tkachenko <hi@sergii.org>
2018-12-29 19:03:07 -0500

    CircleCI: Replace tap-diff with tap-spec

--- test/run-acceptance-with-reports.sh
+++ test/run-acceptance-with-reports.sh
@@ -6,7 +6,8 @@ yarn -s run test:acceptance --tap > ./tmp/tap-ava.txt
 test_exit_code=$?
 
 # Human-readable diff
-FORCE_COLOR=t yarn -s run tap-diff < ./tmp/tap-ava.txt
+# FORCE_COLOR=t yarn -s run tap-diff < ./tmp/tap-ava.txt
+FORCE_COLOR=t yarn -s run tap-spec < ./tmp/tap-ava.txt
 
 # Machine-readable xUnit report
 yarn -s run tap-xunit < ./tmp/tap-ava.txt > ./tmp/tap-xunit.xml
