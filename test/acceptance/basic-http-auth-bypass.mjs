402f496aac64a925f6c3a29aee9a4e2032d9e301
Sergii Tkachenko <hi@sergii.org>
2019-02-02 23:51:54 -0500

    Add red test for auth bypass token

--- test/acceptance/basic-http-auth-bypass.mjs
+++ test/acceptance/basic-http-auth-bypass.mjs
@@ -11,16 +11,29 @@ import { AcceptanceHelpers, BASE_URL_WITH_AUTH } from '../helpers/AcceptanceHelp
 
 const { expect } = chai;
 const { ensureUnauthrozied, withNewPage } = AcceptanceHelpers;
+const AUTH_TOKEN = process.env.PAGERBEAUTY_HTTP_AUTH_TOKEN;
 
 test.beforeEach(AcceptanceHelpers.openBrowser);
 test.afterEach.always(AcceptanceHelpers.closeBrowser);
 
 // ------- Tests ---------------------------------------------------------------
 
-test.serial('HTTP Auth Bypass: Schedule unauthorized without bypass', withNewPage(), async (t, page) => {
+test.serial('HTTP Auth: Schedule P538IZH unauthorized without token', withNewPage(), async (t, page) => {
   const response = await ensureUnauthrozied(page, '/v1/schedules/P538IZH.html');
   // Ensure we're on expected page
   expect(response.url()).to.equal(`${BASE_URL_WITH_AUTH}/v1/schedules/P538IZH.html`);
 });
+
+test.serial.failing(
+  'HTTP Auth: Schedule P538IZH is allowed with auth token',
+  withNewPage(),
+  async (t, page) => {
+    const url = `${BASE_URL_WITH_AUTH}/v1/schedules/P538IZH.html?auth_token=${AUTH_TOKEN}`;
+    const response = await page.goto(url);
+
+    expect(response.ok()).to.be.true;
+    expect(response.status()).to.equal(200);
+  },
+);
 
 // ------- End -----------------------------------------------------------------
