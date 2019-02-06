5241c58c38c61f835a5e4d85ed0e16fbc7b204d8
Sergii Tkachenko <hi@sergii.org>
2019-02-05 22:38:36 -0500

    Apply theme-grafana to oncall view with theme=grafana query parameter

---
+++ test/acceptance/oncall-misc.mjs
@@ -0,0 +1,40 @@
+// ------- Imports -------------------------------------------------------------
+
+import test from 'ava';
+import chai from 'chai';
+
+// ------- Internal imports ----------------------------------------------------
+
+import {
+  AcceptanceHelpers,
+  PageTest,
+  BASE_URL,
+} from '../helpers/AcceptanceHelpers';
+
+// ------- Init ----------------------------------------------------------------
+
+const { expect } = chai;
+const { withNewPage } = AcceptanceHelpers;
+
+test.before(AcceptanceHelpers.openBrowser);
+test.after.always(AcceptanceHelpers.closeBrowser);
+
+// ------- Tests ---------------------------------------------------------------
+
+test('Misc: Grafana theme is applied', withNewPage(), async (t, page) => {
+  const response = await page.goto(`${BASE_URL}/v1/schedules/PJ1P5JQ.html?theme=grafana`);
+
+  await page.waitForSelector('.on_call_root');
+  const pageTest = new PageTest(page);
+  await pageTest.expectClass('.on_call_root', 'theme-grafana');
+});
+
+test('Misc: Other themes is not', withNewPage(), async (t, page) => {
+  const response = await page.goto(`${BASE_URL}/v1/schedules/PJ1P5JQ.html?theme=foo`);
+
+  await page.waitForSelector('.on_call_root');
+  const pageTest = new PageTest(page);
+  await pageTest.expectNoClass('.on_call_root', 'theme-foo');
+});
+
+// ------- End -----------------------------------------------------------------
